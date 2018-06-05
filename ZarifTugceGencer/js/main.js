ab8893dce525959a05d8cab200729bde187a9c6e
Zarif Tuğçe GENÇER <37614650+tugcegencer@users.noreply.github.com>
2018-06-05 10:33:20 +0300

    Add files via upload

---
+++ ZarifTugceGencer/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('.acilanmenu-btn').click(function() {
+
+        var acilanmenu = $('.acilan-menu');
+
+        if(acilanmenu.css('display') == 'none') {
+
+        }
+
+        acilanmenu.slideToggle();
+
+    });
+
+});
